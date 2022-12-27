50dd71c6c0cdff6715e66153c99cad51a882c82b
donghyun3813 <ehdgusdl5753@gmail.com>
2022-12-27 21:36:26 +0900

    클래스 실습 (생성자 오버로딩)

---
+++ src/Chap03/Car.java
@@ -0,0 +1,31 @@
+package Chap03;
+
+public class Car {
+    String company = "현대자도차";
+    String model = "그랜저";
+    String color = "검정";
+    int maxSpeed = 350;
+    int speed;
+    Car(String color, int cc) {
+        this.color = color; // 생성자를 통한 필드값 초기화
+    }
+    // 생성자 오버로딩
+    public Car(){
+
+    }
+
+    public Car(String model) {
+        this.model = model;
+    }
+
+    public Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    public Car(String model, String color, int maxSpeed) {
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+}
---
+++ src/Chap03/Student.java
@@ -0,0 +1,4 @@
+package Chap03;
+
+public class Student {
+}
